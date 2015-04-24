587d96e8c71ccdf95cef604f77fe112cef7d400a
Brandon Kelley <14bmkelley@gmail.com>
2015-04-23 18:52:26 -0700

    moving keylistener into TitleScreen class

--- TitleScreen.java
+++ TitleScreen.java
@@ -1,5 +1,9 @@
 import java.awt.BorderLayout;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
 import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 
@@ -7,6 +11,7 @@ public class TitleScreen extends Screen {
 
 	//field to store information
 	private String title;
+	private int keyDummy;
 
 	//component for field
 	private JLabel titleLabel;
@@ -16,21 +21,28 @@ public class TitleScreen extends Screen {
 
 	//master panel
 	private JPanel corePanel;
+	
+	//listener to register enter keypress
+	private KeyListener keyListener;
 
 	public TitleScreen() {
 		
 		//initialize fields
 		title = "Welcome to Hangman!";
-
-		//initalize component
+		keyDummy = 0;
+		
+		//initialize component
 		titleLabel = new JLabel(title, SwingConstants.CENTER);
-
+		
 		//initialze panel
 		panel = new JPanel();
-
-		//initalize master panel
+		
+		//initialize master panel
 		corePanel = new JPanel();
 		corePanel.setLayout(new BorderLayout());
+		
+		//initialize keylistener
+		setKeyListener();
 		
 	}
 
@@ -38,19 +50,58 @@ public class TitleScreen extends Screen {
 		
 		panel.add(titleLabel);
 		corePanel.add(panel, BorderLayout.CENTER);
+		corePanel.addKeyListener(keyListener);
 		
 	}
 
 	public JPanel getPanel() {
 		
 		return corePanel;
-
+		
 	}
 
 	public void setFocus() {
-
+		
+		corePanel.setFocusable(true);
 		corePanel.requestFocus();
-
+		
+	}
+	
+	private void setKeyListener() {
+	
+		keyListener = new KeyListener() {
+			
+			@Override
+			public void keyPressed(KeyEvent keyEvent) {
+				
+				int keyCode = keyEvent.getKeyCode();
+				
+				if (keyCode == 10 || keyCode == 13) {
+					
+					System.out.println("Hey");
+					Window window = (Window) SwingUtilities.getRoot(corePanel);
+					window.setCurrentScreen(new GameScreen("cat"));
+					
+				}
+				
+			}
+			
+			@Override
+			public void keyReleased(KeyEvent key) {
+				
+				keyDummy++;
+				
+			}
+			
+			@Override
+			public void keyTyped(KeyEvent key) {
+				
+				keyDummy--;
+				
+			}
+			
+		};
+		
 	}
 
 }
--- Window.java
+++ Window.java
@@ -1,103 +1,63 @@
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
-import java.awt.event.KeyListener;
-import java.awt.event.KeyEvent;
 import java.awt.Dimension;
 
-public class Window {
+public class Window extends JFrame {
 
 	private Screen currentScreen;
 	private JPanel currentPanel;
-	private JFrame window;
 	private String word;
-	private KeyListener keyListener;
-	private int keyDummy;
 
 	public Window() {
 		
-		window = new JFrame();
 		word = "";
-		currentScreen = new TitleScreen();
-		keyDummy = 0;
-
+		this.setCurrentScreen(new TitleScreen());
+		
 	}
 
 	public void startGame(String selectedWord) {
 		
 		word = selectedWord;
-		currentScreen.assemble();
-		currentScreen.setFocus();
-
-		setKeyListener();
-		window.addKeyListener(keyListener);
-		window.setFocusable(true);
-		window.requestFocusInWindow();
-
-		currentPanel = currentScreen.getPanel();
-		window.add(currentPanel, BorderLayout.CENTER);
-		window.setPreferredSize(new Dimension(300, 400));
-		window.pack();
-		window.setLocationRelativeTo(null);
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.setVisible(true);
-
+		
+		this.setPreferredSize(new Dimension(300, 400));
+		this.pack();
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+		
 	}
 	
-	private void setKeyListener() {
+	public void setCurrentScreen(Screen newScreen) {
 		
-		keyListener = new KeyListener() {
+		currentScreen = newScreen;
+		
+		if (!(currentScreen instanceof TitleScreen)) {
 			
-			@Override
-			public void keyPressed(KeyEvent keyEvent) {
-				
-				int keyCode = keyEvent.getKeyCode();
-					
-				if (currentScreen instanceof TitleScreen) {
-
-					if (keyCode == 10 || keyCode == 13) {
-
-						currentScreen = new GameScreen(word);
-						window.remove(currentPanel);
-						currentPanel = currentScreen.getPanel();
-						window.add(currentPanel, BorderLayout.CENTER);
-						currentScreen.assemble();
-						window.revalidate();
-						window.repaint();
-						currentScreen.setFocus();
-
-					}
-				
-				} else if (currentScreen instanceof GameScreen) {
-					
-					
-
-				}
-
-			}
-
-			@Override
-			public void keyReleased(KeyEvent key) {
-				
-				keyDummy++;
-
-			}
-	
-			@Override
-			public void keyTyped(KeyEvent key) {
+			this.remove(currentPanel);
 			
-				keyDummy--;
+		}
+		
+		currentPanel = currentScreen.getPanel();
+		this.add(currentPanel, BorderLayout.CENTER);
+		
+		currentScreen.assemble();
+		
+		if (!(currentScreen instanceof TitleScreen)) {
 			
-			}
-
-		};
-	
+			this.revalidate();
+			this.repaint();
+			
+		}
+		
+		currentScreen.setFocus();
+		
 	}
 
 	public static void main(String[] argv) {
-
+		
 		new Window().startGame("cat");
-
+		
 	}
 
 }
