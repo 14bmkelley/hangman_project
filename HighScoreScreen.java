a7ceafe716feb6e15ede7ff97448a0ce7411fb14
Elliot Kirk <elliot.s.kirk@gmail.com>
2015-04-28 11:50:33 -0700

    Added a HighScoreScreen constructor + others
    There is now a HighScoreScreen constructor with a parameter for adding a score to the HighScores txt file. I also added a method addScores that will be used to add the given value from the constructors to the txt file.

--- HighScoreScreen.java
+++ HighScoreScreen.java
@@ -1,12 +1,17 @@
 import java.awt.BorderLayout;
 import java.awt.event.KeyListener;
 import java.awt.event.KeyEvent;
-
 import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.Writer;
+import java.io.File;
+import java.io.IOException;
+
 
 public class HighScoreScreen extends Screen {
     
@@ -22,7 +27,10 @@ public class HighScoreScreen extends Screen {
     //master panel (<-- look at that panel, lording it over there)
     private JPanel corePanel;
     
-	//listener to exit program or restart
+    //score that will be added to the HighScoreScreen
+    private int score;
+    
+    //listener to exit program or restart
 	private KeyListener keyListener;
 
     public HighScoreScreen() {
@@ -43,6 +51,50 @@ public class HighScoreScreen extends Screen {
 		//initialize keylistener
 		setKeyListener();
 
+    }
+    //HighScoreScreen constructor for a high score being passed the final score
+    public HighScoreScreen(int remainingGuesses) {
+        //value to be potentially added to the high score list
+        addScore(remainingGuesses);
+
+        //initialize fields
+        title = "This is the high score screen";
+
+        //initialize component
+        titleLabel = new JLabel(title, SwingConstants.CENTER);
+
+        //initialize panel
+        panel = new JPanel();
+
+        //initialize lord panel
+        corePanel = new JPanel();
+        corePanel.setLayout(new BorderLayout());
+
+    }
+    
+    //Adds the value of the score to the txt file "HighScores.txt"
+    public void addScore(int score){
+        try{
+            String data = " " + String.valueOf(score) + " ";
+
+            File file = new File("HighScores.txt");
+
+            //if file doesnt exists, then create it
+            if(!file.exists()){
+                file.createNewFile();
+            }
+
+            //true = append file
+            FileWriter fileWritter = new FileWriter(file.getName(),true);
+            BufferedWriter bufferWritter = new BufferedWriter(fileWritter);
+            bufferWritter.write(data);
+            bufferWritter.close();
+            System.out.println("Success");
+        }
+        
+        catch(IOException e){
+            e.printStackTrace();
+        }
     }
     
     public void assemble() {
